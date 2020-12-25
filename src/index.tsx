6c259142a6f04605e69f48b479a12e63ac36abf4
Codegem <codegemorg@gmail.com>
2020-12-24 20:34:11 -0500

    Renamed folder

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './extra/reportWebVitals';
+import reportWebVitals from './tests/reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
