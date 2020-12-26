fb7b3051b5d5710cee96a6d4951dcca79c8266c9
Codegem <codegemorg@gmail.com>
2020-12-26 18:39:09 -0500

    Changed toggle styling structure

--- src/Reusable/Toggle/Toggle.tsx
+++ src/Reusable/Toggle/Toggle.tsx
@@ -4,13 +4,15 @@ import styles from './Toggle.module.css';
 
 
 function Toggle({ isON, onToggle, children }: React.PropsWithChildren<{ isON: boolean, onToggle?: () => void }>) {
+    const toggleStyle = isON ? styles.ToggleON : styles.ToggleOFF;
+
     return (
         <div className={styles.Main}>
-            <div className={isON ? styles.Toggle : styles.ToggleOFF} onClick={onToggle}>
+            <div className={`${styles.Toggle} ${toggleStyle}`} onClick={onToggle}>
                 <div className={styles.Internal}>
                 </div>
             </div>
-            
+
             {children}
         </div>
     );
