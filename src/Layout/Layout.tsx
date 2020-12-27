111dc7834cd2466ad91b587a403034209166cb6f
Codegem <codegemorg@gmail.com>
2020-12-27 12:38:59 -0500

    Removed comment in Layout

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -4,28 +4,6 @@ import style from './Layout.module.css';
 import Header from './Header/Header';
 import Panel from '../Panel/Panel';
 
-/**
- * Line 27 uses a Destructured Parameter Type where on the right side you 
- * see the object (in this case it is React.PropsWithChildren<{}>)
- * and on the left side you see the object's propreties listed 
- * in curly braces after a comma { children }
- * 
- * We could write this in a different way as well:
- * 
- *      function Layout(props: React.PropsWithChildren<{}>) {
- *          now you can use `props.children`
- *      }
- * 
- * But we can shorten this to:
- * 
- *      function Layout({ children }: React.PropsWithChildren<{}>)
- * 
- * in that case you access children right away without having to add `props.`
- * --------------
- * 
- * We use PropsWithChildren since we want our Layout component to wrap 
- * another component that will be the `main content`
- **/
 function Layout({ children }: React.PropsWithChildren<{}>) {
     return (
         <div className={style.Layout}>
